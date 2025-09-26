a73cef22f5a3eabae6a15a7f75f0200e8a8ecb6d
Spratch <contact@josephclenet.fr>
2025-09-26 21:04:55 +0200

    fix(data): 1950 end key

--- app/data/uk.ts
+++ app/data/uk.ts
@@ -2,8 +2,9 @@ import events from "@/public/data/uk/events.json";
 import families from "@/public/data/uk/families.json";
 import infos from "@/public/data/uk/infos.json";
 import regimes from "@/public/data/uk/regimes.json";
+import { CountryDataType } from "@/types/countryData";
 
-const data = {
+const data: CountryDataType = {
   events,
   families,
   infos,
